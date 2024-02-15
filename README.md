571f1a4eb9917c262e89579b992a9d6af26603cd
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2024-02-15 19:58:39 +0530

    All Added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 ORM-Suppliementary-Assignment
+
+1)  Query query = session.createQuery("from Book where year(publicationYear)  >= :year");
+    query.setParameter("year", 2010);
+    List<Book> books = query.list();
+    for (Book book : books) {
+       System.out.println("Book ID : " + book.getId());
+       System.out.println("Price : " + book.getPrice());
+       System.out.println("Publication Year : " + book.getPublicationYear());
+       System.out.println("Name : " + book.getTitle() + "\n");
+    }
+2) scs
+3) Query query = session.createQuery("update Book set price = price * 1.1 where author.id IN (:authorID)");
+   query.setParameter("authorID", 2);
+4) Query query = session.createQuery("select avg (price) from Book");
+   Double avg = (Double) query.uniqueResult();
+   System.out.println(avg);
--- src/main/java/lk/ijse/Main.java
+++ src/main/java/lk/ijse/Main.java
@@ -5,10 +5,12 @@ import lk.ijse.entity.Author;
 import lk.ijse.entity.Book;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.sql.Array;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -30,10 +32,16 @@ public class Main {
         book3.setPublicationYear(LocalDate.of(1999, 7, 8));
         book3.setPrice(3190.00);
 
+        Book book4 = new Book();
+        book4.setTitle("Diary of a Wimpy Kid: Long Haul");
+        book4.setPublicationYear(LocalDate.of(2017, 7, 8));
+        book4.setPrice(3190.00);
+
         ArrayList<Book> bookList = new ArrayList<>();
         bookList.add(book1);
         bookList.add(book2);
         bookList.add(book3);
+        bookList.add(book4);
 
         Author author1 = new Author();
         author1.setName("Hergé");
@@ -53,13 +61,33 @@ public class Main {
         book1.setAuthor(author1);
         book2.setAuthor(author2);
         book3.setAuthor(author3);
+        book4.setAuthor(author2);
 
-        session.save(author1);
+        /*session.save(author1);
         session.save(book1);
         session.save(author2);
         session.save(book2);
         session.save(author3);
         session.save(book3);
+        session.save(book4);*/
+
+        /*Query query = session.createQuery("from Book where year(publicationYear)  >= :year");
+        query.setParameter("year", 2010);
+        List<Book> books = query.list();
+        for (Book book : books) {
+            System.out.println("Book ID : " + book.getId());
+            System.out.println("Price : " + book.getPrice());
+            System.out.println("Publication Year : " + book.getPublicationYear());
+            System.out.println("Name : " + book.getTitle() + "\n");
+        }*/
+
+        /*Query query = session.createQuery("update Book set price = price * 1.1 where author.id IN (:authorID)");
+        query.setParameter("authorID", 2);*/
+
+        Query query = session.createQuery("select avg (price) from Book");
+        Double avg = (Double) query.uniqueResult();
+        System.out.println(avg);
+
 
         transaction.commit();
         session.close();
